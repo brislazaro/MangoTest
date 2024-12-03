1a0cc6d0c72863f32a4dec1e2b797bc7ff57c01a
brislazaro <brisa.lazaroc@gmail.com>
2024-12-03 17:58:05 +0100

    create app css and layout styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import "./app.css";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html className="container" lang="en">
       <body>{children}</body>
     </html>
   );
