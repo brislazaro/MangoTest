a08d05a5b454d8e5eb6919f6168ea0d9574228bd
brislazaro <brisa.lazaroc@gmail.com>
2024-12-03 18:20:00 +0100

    link styles like button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
+import "./app.css";
 
 export default function Page() {
-  return <Link href="/exercise1">Go to Exercise 1</Link>;
+  return (
+    <>
+      <Link className="button" href="/exercise1">
+        Go to Exercise 1
+      </Link>
+    </>
+  );
 }
