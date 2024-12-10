bb6c1e95adbcbe0b80463adc426376388fe8ac37
brislazaro <brisa.lazaroc@gmail.com>
2024-12-10 17:37:12 +0100

    create fixedrange component

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import "./app.css";
 
 export default function Page() {
   return (
-    <>
+    <div className="buttons-container">
       <Link className="button" href="/exercise1">
         Go to Exercise 1
       </Link>
-    </>
+      <Link className="button" href="/exercise2">
+        Go to Exercise 2
+      </Link>
+    </div>
   );
 }
